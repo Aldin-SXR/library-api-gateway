ce5421855132ca31cab1919c93953136e1a01c73
Aldin Kovačević <aldin@salesseek.com>
2023-11-16 09:09:17 +0100

    Initial commit.

---
+++ src/test/java/ba/edu/ibu/libraryapigateway/LibraryApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.libraryapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
